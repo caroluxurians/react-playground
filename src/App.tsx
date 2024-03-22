876a284fe212732d8dca4aacfed9f1f89cee3fb4
Žofie Tůmová <zofietumova@gmail.com>
2024-03-22 17:39:50 +0100

    move buttons

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const Board = ({ xIsNext, squares, onPlay }: BoardProps) => {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
-  }
+  };
 
   const winner = calculateWinner(squares);
   let status;
@@ -71,6 +71,24 @@ const Game = () => {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   };
+
+  const jumpTo(nextMove) {
+    //
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = `Go to game start`;
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
