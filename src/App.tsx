7ac77b23a54100944406d8215b8a9ab6f18fde3c
Žofie Tůmová <zofietumova@gmail.com>
2024-03-26 14:09:04 +0100

    types

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,28 @@ type SquareProps = {
 };
 type BoardProps = {
   xIsNext: boolean
-  squares: Array
-  onPlay: () => void
+  squares: string[]
+  onPlay: (squares: string[]) => void
+};
+
+const calculateWinner = (squares: string[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 const Square = ({ value, onSquareClick }: SquareProps) => {
@@ -68,13 +88,13 @@ const Game = () => {
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  const handlePlay = (nextSquares) => {
+  const handlePlay = (nextSquares: string[]) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   };
 
-  const jumpTo = (nextMove) => {
+  const jumpTo = (nextMove: number) => {
     setCurrentMove(nextMove);
   };
 
@@ -86,6 +106,7 @@ const Game = () => {
       description = "Go to game start";
     }
     return (
+      // eslint-disable-next-line react/no-array-index-key
       <li key={move}>
         <button type="button" onClick={() => jumpTo(move)}>{description}</button>
       </li>
@@ -102,26 +123,6 @@ const Game = () => {
       </div>
     </div>
   );
-};
-
-const calculateWinner = (squares) => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i += 1) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 };
 
 export default Game;
